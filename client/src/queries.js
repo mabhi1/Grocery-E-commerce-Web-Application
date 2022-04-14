46e633aefebd4811e24f826d03361111973c6db5
shrey-shahh <sshah4@stevens.edu>
2022-04-14 15:37:31 -0400

    'shrey'

---
+++ client/src/queries.js
@@ -0,0 +1,53 @@
+import {gql} from '@apollo/client';
+
+const GET_PRODUCTS_NAME_PRICE = gql`
+    query{
+        products{
+            _id
+            name
+            price
+        }
+    }
+`
+const GET_PRODUCTS_BY_ID = gql`
+    query{
+        product(id: $id){
+            _id
+            name
+            description
+            price
+            quantity
+        }
+    }
+`
+
+const ADD_PRODUCT = gql`
+    mutation createProduct(
+        $name: String!
+        $description: String
+        $price: Int!
+        $quantity: Int!
+        ){
+        addProdcut(
+            name: $name!
+            description: $description
+            price: $price
+            quantity: $quantity
+            ){
+                _id
+                name
+                description
+                price
+                quantity
+            }
+        }
+    }
+`
+
+let exported = {
+    GET_PRODUCTS_BY_ID,
+    GET_PRODUCTS_NAME_PRICE,
+    ADD_PRODUCT
+}
+
+export default exported;
