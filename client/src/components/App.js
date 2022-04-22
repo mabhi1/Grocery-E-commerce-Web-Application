d4f1faff42c993fa759a50c9778099e32a3987f8
akotian24 <akotian@stevens.edu>
2022-04-22 17:11:31 -0400

    nav changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,13 @@ import "../App.css";
 import Navigation from "./Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
-import Home from "./Home";
+//import Home from "./Home";
 import Account from "./Account";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import { AuthProvider } from "../Firebase/Auth";
 import PrivateRoute from "./PrivateRoute";
+import Products from "./Products";
 
 function App() {
     return (
@@ -16,11 +17,11 @@ function App() {
                 <div className="App">
                     <Navigation />
                     <Routes>
-                        <Route exact path="/" element={<Landing />} />
+                        <Route exact path="/" element={<Products />} />
                         <Route exact path="/signin" element={<SignIn />} />
                         <Route exact path="/signup" element={<SignUp />} />
                         <Route element={<PrivateRoute />}>
-                            <Route exact path="/home" element={<Home />} />
+                            {/* <Route exact path="/home" element={<Home />} /> */}
                             <Route exact path="/account" element={<Account />} />
                         </Route>
                     </Routes>
