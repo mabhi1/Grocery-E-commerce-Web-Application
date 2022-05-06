5289ea70ec832e866591bc73113d5d879c63ac56
swom <swayamshah97@gmail.com>
2022-05-06 03:31:48 -0400

    swom

--- client/src/components/Cart/Success.js
+++ client/src/components/Cart/Success.js
@@ -1,10 +1,10 @@
 import React from "react";
-import AddOrder from "../orders/AddOrder";
+
 
 function Success() {
     console.log('Im here')
     return <div>
-        <AddOrder /></div>;
+        </div>;
 }
 
 export default Success;
