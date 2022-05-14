120991467c547c2507d5ad8e92c9e4f8ae8f6e5e
swom <swayamshah97@gmail.com>
2022-05-14 16:39:06 -0400

    swom

--- client/src/components/Cart/Failed.js
+++ client/src/components/Cart/Failed.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery } from "@apollo/client";
 import queries from "../../queries";
-import { Button, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function Failed() {
     const logo = require("../../assets/fail.gif"); 
@@ -23,7 +23,7 @@ function Failed() {
         return (
             <div>
                 
-                <img alt="Success Image" id="logo" src={logo} style={{ width: "45%", height: "45%" , marginTop: "30px"}} />
+                <img alt="Success" id="logo" src={logo} style={{ width: "45%", height: "45%" , marginTop: "30px"}} />
                 <br />
                 <br/>
                 
