7321a2e6a3e96e1c17e3c4adc9f1a0c62a4ee751
Abhishek Mishra <amishr1@stevens.edu>
2023-01-05 02:13:28 -0500

    deployed on render

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,47 +22,47 @@ import Checkout from "./Cart/Checkout";
 import Footer from "./Footer";
 
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: "http://localhost:4000/",
-    }),
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://asian-mart-apollo.onrender.com",
+  }),
 });
 
 function App() {
-    return (
-        <AuthProvider>
-            <ApolloProvider client={client}>
-                <Router>
-                    <div className="App">
-                        <Navigation />
-                        <Routes>
-                            <Route path="/" element={<Landing />} />
-                            <Route path="/signin" element={<SignIn />} />
-                            <Route path="/signup" element={<SignUp />} />
-                            <Route path="/products/:pageNum" element={<Products />} />
-                            <Route path="/notfound" element={<NotFound />} />
-                            <Route path="/product/:id" element={<IndividualProduct />} />
-                            <Route path="/forgot-password" element={<ForgotPassword />} />
-                            <Route path="*" element={<NotFound />} />
-                            <Route element={<PrivateRoute />}>
-                                <Route path="/update-profile" element={<UpdateProfile />} />
-                                <Route path="/paymentsuccess" element={<Success />} />
-                                <Route path="/paymentsuccess/:secret" element={<Success />} />
-                                <Route path="/paymentfailed" element={<Failed />} />
-                                <Route path="/paymentfailed/:secret" element={<Failed />} />
-                                <Route path="/cart" element={<Cart />} />
-                                <Route path="/userDetail" element={<UserDetailPage />} />
-                                <Route path="/account" element={<Account />} />
-                                <Route path="/admin" element={<Admin />} />
-                                <Route path="/checkout" element={<Checkout />} />
-                            </Route>
-                        </Routes>
-                    </div>
-                    <Footer />
-                </Router>
-            </ApolloProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="App">
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/products/:pageNum" element={<Products />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/product/:id" element={<IndividualProduct />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="*" element={<NotFound />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/update-profile" element={<UpdateProfile />} />
+                <Route path="/paymentsuccess" element={<Success />} />
+                <Route path="/paymentsuccess/:secret" element={<Success />} />
+                <Route path="/paymentfailed" element={<Failed />} />
+                <Route path="/paymentfailed/:secret" element={<Failed />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/userDetail" element={<UserDetailPage />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/checkout" element={<Checkout />} />
+              </Route>
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </ApolloProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
